4f6d7080d7070688d6b0a6750768bc618839ec74
naveenkumar <naveenmallapuraju@gmail.com>
2021-05-19 13:05:20 +0530

    Commit message

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string FirstName;
+            string age;
+
+
+            Console.WriteLine("what is Your Name");
+            FirstName = Console.ReadLine();
+
+            Console.WriteLine("What is your age");
+            age = Console.ReadLine();
+
+
+            //string response = "Your name is" + FirstName + "And your age is " + age;
+
+            string response = $"Your name is {FirstName} and your age is {age}";
+
+            Console.WriteLine(response);
+
+
+
+        }
+
+    }
+    }
+
